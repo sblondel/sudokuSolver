48681208c73d3571d5b9e039a10cb319dd70ee39
Sophie Blondel [bqo] <sophie@lap98867.ornl.gov>
2015-08-28 17:59:35 -0400

    One algorithm has been added for harder grids, but the solver will need more algorithm.

--- Grid.py
+++ Grid.py
@@ -68,6 +68,86 @@ class Grid:
             if (nWhites > 1 and sameRow):
                 self.makeRowExcludeBlocGrey(rowID, i)
 
+    def algorithmOne(self):
+        conti = False
+        standard = range(1, 10)
+        for i in range(0, 9):
+            column = self.columnList[i]
+            columnPresent = []
+            row = self.rowList[i]
+            rowPresent = []
+            for j in range(0, 9):
+                box = self.boxList[column.boxList[j]]
+                if (box.color == "black"):
+                    columnPresent.append(box.value)
+                box = self.boxList[row.boxList[j]]
+                if (box.color == "black"):
+                    rowPresent.append(box.value)
+            columnMissing = list(set(standard) - set(columnPresent))
+            rowMissing = list(set(standard) - set(rowPresent))
+            for j in range(0, 9):
+                bloc = self.blocList[j]
+                
+                if (self.blocPartOfColumn(j, i)):
+                    blocPresent = []
+                    for k in range(0, 9):
+                        box = self.boxList[bloc.boxList[k]]
+                        if (box.color == "black"):
+                            blocPresent.append(box.value)
+                    intersection = self.intersect(blocPresent, columnMissing)
+                    outsideBlocEmpty = []
+                    for k in range(0, 9):
+                        box = self.boxList[column.boxList[k]]
+                        if (box.value == 0 and not self.boxInBloc(box.index, bloc.index)):
+                            outsideBlocEmpty.append(box.index)
+                    if (len(outsideBlocEmpty) == len(intersection)):
+                        values = list(set(columnMissing) - set(intersection))
+                        for k in range(0, len(values)):
+                            self.makeGridGrey(values[k])
+                            for l in range(0, len(outsideBlocEmpty)):
+                                box = self.boxList[outsideBlocEmpty[l]]
+                                box.makeBoxGrey()
+                            if (self.fillGrid(values[k])):
+                                conti = True
+                            
+                if (self.blocPartOfRow(j, i)):
+                    blocPresent = []
+                    for k in range(0, 9):
+                        box = self.boxList[bloc.boxList[k]]
+                        if (box.color == "black"):
+                            blocPresent.append(box.value)
+                    intersection = self.intersect(blocPresent, rowMissing)
+                    outsideBlocEmpty = []
+                    for k in range(0, 9):
+                        box = self.boxList[row.boxList[k]]
+                        if (box.value == 0 and not self.boxInBloc(box.index, bloc.index)):
+                            outsideBlocEmpty.append(box.index)
+                    if (len(outsideBlocEmpty) == len(intersection)):
+                        values = list(set(rowMissing) - set(intersection))
+                        for k in range(0, len(values)):
+                            self.makeGridGrey(values[k])
+                            for l in range(0, len(outsideBlocEmpty)):
+                                box = self.boxList[outsideBlocEmpty[l]]
+                                box.makeBoxGrey()
+                            if (self.fillGrid(values[k])):
+                                conti = True
+        return conti
+    
+    def intersect(self, a, b):
+        return list(set(a) & set(b))
+                    
+    def blocPartOfColumn(self, blocID, columnID):
+        if (blocID % 3 == int(columnID/3)): 
+            return True
+        else:
+            return False
+        
+    def blocPartOfRow(self, blocID, rowID):
+        if (int(blocID/3) == int(rowID/3)): 
+            return True
+        else:
+            return False
+
     def boxInBloc(self, boxID, blocID):
         isIn = False
         for i in range(0, 9):
@@ -98,56 +178,62 @@ class Grid:
     def makeBlocGrey(self, id):
         for j in range(0, 9):
             self.boxList[self.blocList[id].boxList[j]].makeBoxGrey()
+            
+    def fillGrid(self, value):
+        conti = False
+        for j in range(0, 9):
+            column = self.columnList[j]
+            row = self.rowList[j]
+            bloc = self.blocList[j]
+            nColumnWhite = 0
+            nRowWhite = 0
+            nBlocWhite = 0
+            columnBoxID = -1
+            rowBoxID = -1
+            blocBoxID = -1
+            for k in range(0, 9):
+                if (self.boxList[column.boxList[k]].color == "white"):
+                    nColumnWhite = nColumnWhite + 1
+                    columnBoxID = column.boxList[k]
+                if (self.boxList[row.boxList[k]].color == "white"):
+                    nRowWhite = nRowWhite + 1
+                    rowBoxID = row.boxList[k]
+                if (self.boxList[bloc.boxList[k]].color == "white"):
+                    nBlocWhite = nBlocWhite + 1
+                    blocBoxID = bloc.boxList[k]
 
+            if (nColumnWhite == 1):
+                self.boxList[columnBoxID].setValue(value)
+                self.makeColumnGrey(self.boxList[columnBoxID].column)
+                self.makeRowGrey(self.boxList[columnBoxID].row)
+                self.makeBlocGrey(self.boxList[columnBoxID].bloc)
+                conti = True
+            if (nRowWhite == 1):
+                self.boxList[rowBoxID].setValue(value)
+                self.makeColumnGrey(self.boxList[rowBoxID].column)
+                self.makeRowGrey(self.boxList[rowBoxID].row)
+                self.makeBlocGrey(self.boxList[rowBoxID].bloc)
+                conti = True
+            if (nBlocWhite == 1):
+                self.boxList[blocBoxID].setValue(value)
+                self.makeColumnGrey(self.boxList[blocBoxID].column)
+                self.makeRowGrey(self.boxList[blocBoxID].row)
+                self.makeBlocGrey(self.boxList[blocBoxID].bloc)
+                conti = True
+
+        self.reSet()
+        return conti
+        
     def solve(self):
         conti = True
         while (conti):
             conti = False
             for i in range(1, 10):
                 self.makeGridGrey(i)
-                for j in range(0, 9):
-                    column = self.columnList[j]
-                    row = self.rowList[j]
-                    bloc = self.blocList[j]
-                    nColumnWhite = 0
-                    nRowWhite = 0
-                    nBlocWhite = 0
-                    columnBoxID = -1
-                    rowBoxID = -1
-                    blocBoxID = -1
-                    for k in range(0, 9):
-                        if (self.boxList[column.boxList[k]].color == "white"):
-                            nColumnWhite = nColumnWhite + 1
-                            columnBoxID = column.boxList[k]
-                        if (self.boxList[row.boxList[k]].color == "white"):
-                            nRowWhite = nRowWhite + 1
-                            rowBoxID = row.boxList[k]
-                        if (self.boxList[bloc.boxList[k]].color == "white"):
-                            nBlocWhite = nBlocWhite + 1
-                            blocBoxID = bloc.boxList[k]
-
-                    if (nColumnWhite == 1):
-                        self.boxList[columnBoxID].setValue(i)
-                        self.makeColumnGrey(self.boxList[columnBoxID].column)
-                        self.makeRowGrey(self.boxList[columnBoxID].row)
-                        self.makeBlocGrey(self.boxList[columnBoxID].bloc)
-                        conti = True
-                    if (nRowWhite == 1):
-                        self.boxList[rowBoxID].setValue(i)
-                        self.makeColumnGrey(self.boxList[rowBoxID].column)
-                        self.makeRowGrey(self.boxList[rowBoxID].row)
-                        self.makeBlocGrey(self.boxList[rowBoxID].bloc)
-                        conti = True
-                    if (nBlocWhite == 1):
-                        self.boxList[blocBoxID].setValue(i)
-                        self.makeColumnGrey(self.boxList[blocBoxID].column)
-                        self.makeRowGrey(self.boxList[blocBoxID].row)
-                        self.makeBlocGrey(self.boxList[blocBoxID].bloc)
-                        conti = True
-
-                self.reSet()
-
-        print "Done!"
+                if (self.fillGrid(i)):
+                    conti = True
+        if (self.algorithmOne()):
+            self.solve()
 
     def printGrid(self):
         for i in range(0, 9):
